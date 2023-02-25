d3ed0487ac2a77814b29b16a34064cea2f20c939
Mobydack <devzetsubou@gmail.com>
2023-02-25 13:03:20 +0700

    chore: reviving this repo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,27 @@
 module.exports = {
-  root: true,
-  env: {
-    es2021: true
-  },
-  extends: ['standard', 'prettier'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module'
-  },
-  plugins: ['@typescript-eslint'],
-  rules: {
-    semi: ['error', 'always']
-  },
-  overrides: [
-    {
-      files: '*.test.ts',
-      env: { 'jest/globals': true},
-      extends: ['plugin:jest/all']
-    }
-  ]
+    root: true,
+    env: {
+        node: true,
+        es2021: true,
+    },
+    extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+    parserOptions: {
+        ecmaVersion: 12,
+    },
+    overrides: [
+        {
+            files: ['src/**/*.ts'],
+            extends: ['plugin:@typescript-eslint/recommended'],
+            parser: '@typescript-eslint/parser',
+            parserOptions: {
+                sourceType: 'module',
+            },
+            plugins: ['@typescript-eslint'],
+        },
+        {
+            files: 'src/**/__tests__/**',
+            env: { 'jest/globals': true },
+            extends: ['plugin:jest/all'],
+        },
+    ],
 };
